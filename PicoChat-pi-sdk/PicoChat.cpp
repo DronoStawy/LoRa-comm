6a1d8c3cf5047e392bec8517aa44c87a216ab176
Jakub Siciarek <jsiciar@icloud.com>
2025-05-22 10:34:17 +0200

    SDK version rewrite start

--- PicoChat-pi-sdk/PicoChat.cpp
+++ PicoChat-pi-sdk/PicoChat.cpp
@@ -1,3 +1,15 @@
+/*
+  TO DO
+  - password as the only thing needed for creating chat and joining --------- []
+  - messages many to many with retransmission ------------------------------- []
+  - visible user status (obline/offline) ------------------------------------ []
+  - read indication --------------------------------------------------------- []
+  - CSMA - Carrier-Sense Multiple Access ------------------------------------ [] https://nws.sg/amalinda/lmac/
+  - encrypted messages ------------------------------------------------------ []
+  - ACK for receiving message and changing channel/spreading factor --------- []
+  - FHSS - frequency hopping ------------------------------------------------ []
+*/
+
 #include <pico/stdlib.h>
 #include "hardware/watchdog.h"
 #include <RadioLib.h>
@@ -16,9 +28,36 @@
 
 #define MODE_PIN 7
 
+// Set ACK timeout to 2 seconds
+#define ACK_TIMEOUT_MS 2000
+
+// ACK timing variables
+uint32_t curr_ack_check_time = 0;
+uint32_t prev_ack_check_time = 0;
+
+// Heartbeat timing variables
+uint32_t curr_heartbeat_time = 0;
+uint32_t prev_heartbeat_time = 0;
+
 // create a new instance of the HAL class
 PicoHal *hal = new PicoHal(spi1, LORA_MISO, LORA_MOSI, LORA_SCK);
 SX1262 radio = new Module(hal, LORA_CS, LORA_DIO1, LORA_RST, LORA_BUSY);
+
+volatile bool interrupt_flag = false;
+bool waiting_for_packet_flag = false;
+bool waiting_for_ack_flag = false;
+bool sending_packet_flag = false;
+bool sending_ack_flag = false;
+
+void intFlag()
+{
+  interrupt_flag = true;
+}
+
+// Function prototypes
+int radioInit();
+int sendMessage(char *usr_name, char *message);
+int checkState(int state);
 
 int main()
 {
@@ -26,9 +65,85 @@ int main()
 
   gpio_init(PICO_DEFAULT_LED_PIN);
   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+  uart_set_hw_flow(uart0, false, false);
+
   bool led_state = false;
 
-  printf("[SX1262] Initializing ... ");
+  radioInit();
+  chat_stage stage = IDLE;
+
+  for (;;)
+  {
+    switch (stage)
+    {
+    case IDLE:
+    {
+      if (!waiting_for_packet_flag)
+      {
+        int state = radio.startReceive();
+        checkState(state);
+        waiting_for_packet_flag = true;
+      }
+      if (interrupt_flag)
+      {
+        printf("Packet received!\n");
+        uint8_t buf[255];
+        int state = radio.readData(buf, sizeof(buf));
+        if (checkState(state))
+        {
+          chat_packet packet(buf);
+          printf("Packet id: %s\n", packet.id);
+          printf("User name: %s\n", packet.user_name);
+
+          if(packet.id == PACKET_TYPE_HEARTBEAT){
+            printf("Heartbeat received from %s\n", packet.user_name);
+            interrupt_flag = false;
+            stage = IDLE;
+            waiting_for_packet_flag = false;
+            break;
+          }
+
+          else if (packet.id == PACKET_TYPE_MESSAGE)
+          {
+            printf("Message: %s\n", packet.message);
+            interrupt_flag = false;
+            stage = SENDING_ACK;
+            waiting_for_packet_flag = false;
+            break;
+          }
+          else if (packet.id == PACKET_TYPE_ACK)
+          {
+            printf("ACK received from %s\n", packet.user_name);
+            interrupt_flag = false;
+            stage = IDLE;
+            waiting_for_packet_flag = false;
+            break;
+          }
+          else
+          {
+            printf("Unknown packet type\n");
+            interrupt_flag = false;
+            stage = IDLE;
+            waiting_for_packet_flag = false;
+            break;
+          }
+        }
+      }
+    }
+
+    break;
+
+    default:
+      break;
+    }
+  }
+  return (0);
+}
+
+int radioInit()
+{
+  // initialize the radio
   int state = radio.begin(868.0, 125.0, 9, 7, RADIOLIB_SX126X_SYNC_WORD_PRIVATE, 14, 8, 0, true);
   if (state != RADIOLIB_ERR_NONE)
   {
@@ -37,27 +152,27 @@ int main()
     return state;
   }
   printf("success!\n");
+  return RADIOLIB_ERR_NONE;
 
-  for (;;)
+  radio.setDio1Action(intFlag);
+  radio.setOutputPower(0);
+}
+
+int sendMessage(char *usr_name, char *message)
+{
+  // create a new packet
+  chat_packet packet(PACKET_TYPE_MESSAGE, usr_name, message);
+  radio.transmit(packet.toByteArray(), packet.getPacketSize());
+}
+
+int checkState(int state)
+{
+  if (state != RADIOLIB_ERR_NONE)
   {
+    printf("failed, code %d\n", state);
     gpio_put(PICO_DEFAULT_LED_PIN, 1);
-    // send a packet
-    message_packet packet(1, "RP2040", "Hello from pico SDK!");
-    printf("[SX1262] Transmitting packet ... ");
-    state = radio.transmit(packet.toByteArray(), packet.getPacketSize());
-    if(state == RADIOLIB_ERR_NONE) {
-      // the packet was successfully transmitted
-      printf("success!\n");
-
-      // wait for a second before transmitting again
-      gpio_put(PICO_DEFAULT_LED_PIN, 0);
-      hal->delay(2000);
-
-    } else {
-      printf("failed, code %d\n", state);
-
-    }
+    return state;
   }
-
-  return (0);
+  printf("success!\n");
+  return RADIOLIB_ERR_NONE;
 }
--- PicoChat-pi-sdk/packets/packets.hpp
+++ PicoChat-pi-sdk/packets/packets.hpp
@@ -1,21 +1,46 @@
 #include <pico/stdlib.h>
 #include <cstring>
 #include <cstdlib>
+#include <RadioLib.h>
 
 #define MAX_USER_NAME_LENGTH 8
 #define MAX_MESSAGE_LENGTH 252 - MAX_USER_NAME_LENGTH // 256 byte max Lora packet size - 1 byte for id - 1 byte for length - 8 bytes for username - 1 byte for user name length
 
+#define PACKET_TYPE_HEARTBEAT 0
+#define PACKET_TYPE_ACK 1
+#define PACKET_TYPE_MESSAGE 2
+
 // Default message packet structure
-struct message_packet
+/*
+ @brief Structure representing a PicoChat packet.
+*/
+
+enum chat_stage
+{
+  /*
+  */
+  IDLE,                 // Waiting for heartbeats from other users and periodically sending own one
+  SENDING_HEARTBEAT,    // Sending heartbeat to other users
+  WAITING_FOR_PACKET,   //
+  SENDING_PACKET, 
+  WAITING_FOR_ACK,
+  SENDING_ACK,
+};
+
+
+struct chat_packet
 {
 public:
-  uint8_t id; // 0 - ack message
+  uint8_t id; // 0 - heartbeat, 1 - ack, 2 - message
   uint8_t user_name_length;
   uint8_t message_length;
   char *user_name;
   char *message;
 
-  message_packet(uint8_t id, const char *usr_name, const char *text)
+  /*
+  @param id Packet ID
+  */
+  chat_packet(uint8_t id, const char *usr_name, const char *text)
   {
     this->id = id;
     this->user_name_length = (strlen(usr_name) > MAX_USER_NAME_LENGTH) ? MAX_USER_NAME_LENGTH : strlen(usr_name);
@@ -26,15 +51,13 @@ public:
     memcpy(message, text, message_length);
   }
 
-  message_packet(uint8_t *buf)
+  chat_packet(uint8_t *buf)
   {
     this->id = buf[0];
     this->user_name_length = buf[1];
     this->message_length = buf[2];
     user_name = (char *)malloc(user_name_length + 1);
     message = (char *)malloc(message_length + 1);
-    // memcpy(user_name, &buf[3], user_name_length);
-    // memcpy(message, &buf[3 + user_name_length], message_length);
     memcpy(user_name, &buf[3], user_name_length);
     memcpy(message, &buf[3 + user_name_length], message_length);
     user_name[user_name_length] = '\0';
@@ -64,7 +87,7 @@ public:
     return buffer;
   }
 
-  ~message_packet()
+  ~chat_packet()
   {
     free(user_name);
     free(message);
