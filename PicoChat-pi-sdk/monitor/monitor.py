40bf0a2fe5226cfa9b67ea926f4ce32b6fc26a53
Jakub Siciarek <jsiciar@icloud.com>
2025-06-01 22:04:35 +0200

    Monitor app prototype

--- PicoChat-pi-sdk/monitor/monitor.py
+++ PicoChat-pi-sdk/monitor/monitor.py
@@ -1,8 +1,21 @@
+import sys
+import random
 import serial
-import PySide6.QtCore
+from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtUiTools import QUiLoader
 
-# Prints PySide6 version
-print(PySide6.__version__)
-
-# Prints the Qt version used to compile PySide6
-print(PySide6.QtCore.__version__)
+if __name__ == '__main__':
+    # Some code to obtain the form file name, ui_file_name
+    app = QtWidgets.QApplication(sys.argv)
+    ui_file = QtCore.QFile("monitor/monitor.ui")
+    if not ui_file.open(QtCore.QIODevice.ReadOnly):
+        print("Cannot open {}: {}".format("monitor.ui", ui_file.errorString()))
+        sys.exit(-1)
+    loader = QUiLoader()
+    widget = loader.load(ui_file, None)
+    ui_file.close()
+    if not widget:
+        print(loader.errorString())
+        sys.exit(-1)
+    widget.show()
+    sys.exit(app.exec_())
